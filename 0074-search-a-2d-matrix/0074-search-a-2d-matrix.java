d3fcc4b1757a624eff822932e194b4bd8f15c2d7
Abhinav Raman <abhinavraman1203@gmail.com>
2023-10-14 21:17:49 +0530

    Time: 0 ms (100%), Space: 40.8 MB (59.94%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int X) {
+        int N=mat.length;
+        int M=mat[0].length;
+       int i=0,j=M-1;
+        while(i<N && j>=0){
+            if(mat[i][j]==X){
+                return true;
+            }
+            else if(mat[i][j]<X){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return false;
+    }
+}

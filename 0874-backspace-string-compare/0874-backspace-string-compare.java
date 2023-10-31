9eba40709184da072418b90229b2de335409967c
Abhinav Raman <abhinavraman1203@gmail.com>
2023-11-01 00:52:58 +0530

    Time: 2 ms (53.85%), Space: 40.6 MB (52.3%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -1,18 +1,20 @@
-class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        return sb(s).equals(sb(t));
+    class Solution{
+    public boolean backspaceCompare(String S, String T) {
+        return stackSolution(S).equals(stackSolution(T));
     }
-    public String sb(String str){
-        StringBuilder sbr=new StringBuilder();
-
-        for(char c:str.toCharArray()){
-            if(c!='#'){
-                sbr.append(c);
-            }
-            else if(sbr.length()!=0){ 
-                sbr.deleteCharAt(sbr.length()-1);
+    
+    private String stackSolution(String str) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (char c : str.toCharArray()) {
+		
+            if (c != '#') {
+                stack.push(c);
+				
+            } else if (!stack.isEmpty()) {
+                stack.pop();
             }
         }
-        return sbr.toString();
+        return stack.toString();
     }
-}
+    }

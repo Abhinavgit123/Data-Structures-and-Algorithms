2900bc71610dac93e6711454f81caa268d186613
Abhinav Raman <abhinavraman1203@gmail.com>
2023-11-01 19:51:00 +0530

    Time: 1 ms (97.04%), Space: 40.8 MB (52.82%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        if(word.length() < 2) return true;
+        if(word.length()<2) return true;
         if(Character.isUpperCase(word.charAt(0)) && Character.isUpperCase(word.charAt(1))){
+            // This is for fully upper case characters
             for(int i = 1; i < word.length(); i++){
                 if(Character.isLowerCase(word.charAt(i))) return false;
             }
@@ -12,6 +13,7 @@ class Solution {
                 if(Character.isUpperCase(word.charAt(i))) return false;
             }
         }
+        // else if 1st letter is uppercase and rest is lower case it will come here
         return true;
     }
 }

27bf89186aed8dbedf8d9c0ca996b8d7165e1502
Abhinav Raman <abhinavraman1203@gmail.com>
2024-10-18 16:06:48 +0530

    Time: 3 ms (99.90%) | Memory: 55.1 MB (71.03%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,28 +1,19 @@
 class Solution {
-
     public int maxWidthRamp(int[] nums) {
-        int n = nums.length;
-        int[] rightMax = new int[n];
-
-        // Fill rightMax array with the maximum values from the right
-        rightMax[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        int n=nums.length;
+        int[] maxarr=new int[n];
+        maxarr[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            maxarr[i]=Math.max(nums[i],maxarr[i+1]);
         }
-
-        int left = 0, right = 0;
-        int maxWidth = 0;
-
-        // Traverse the array using left and right pointers
-        while (right < n) {
-            // Move left pointer forward if current left exceeds rightMax
-            while (left < right && nums[left] > rightMax[right]) {
+        int left=0,right=0,maxwidth=0;
+        while(right<n){
+            while(left<right && nums[left]>maxarr[right]){
                 left++;
             }
-            maxWidth = Math.max(maxWidth, right - left);
+            maxwidth=Math.max(maxwidth,right-left);
             right++;
         }
-
-        return maxWidth;
+        return maxwidth;
     }
 }
